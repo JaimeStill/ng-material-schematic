71acdfae0d02d0eeee42ba3d7de9314f85ba7d36
Jaime Still <jpstill85@gmail.com>
2021-08-20 06:54:19 -0400

    pastel palette colors + background-stacked -> background-default + lib component config

--- src/app/files/projects/__library@dasherize__/src/lib/__library@dasherize__.module.ts
+++ src/app/files/projects/__library@dasherize__/src/lib/__library@dasherize__.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 
+import { Components } from './components';
 import { Dialogs } from './dialogs';
 import { Directives } from './directives';
 import { Pipes } from './pipes';
 
 @NgModule({
   declarations: [
+    ...Components,
     ...Dialogs,
     ...Directives,
     ...Pipes
   ],
   imports: [
+    CommonModule,
     MaterialModule
   ],
   exports: [
+    ...Components,
     ...Dialogs,
     ...Directives,
     ...Pipes,
